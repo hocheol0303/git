c4aa936e815439a58b9b56d8731b529716b331a7
hocheol0303 <136863755+hocheol0303@users.noreply.github.com>
2024-09-10 09:19:15 +0900

    [Gold IV] Title: 키 순서, Time: 2524 ms, Memory: 114140 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -0,0 +1,35 @@
+'''
+컨닝 딱 대
+플로이드 워셜 : 그래프에서 가능한 모든 노드 쌍에 대해 최단거리를 구하는 알고리즘
+    3중 for문으로 (출발지-도착지), (출발지-경유지)+(경유지-도착지) 중 최단거리를 비교하는 것
+'''
+
+import sys
+
+n, m = map(int,sys.stdin.readline().split())
+
+grp=[[float('inf') for j in range(n+1)] for i in range(n+1)]
+
+for i in range(m):
+    start, end = map(int , sys.stdin.readline().split())
+    grp[start][end]=1
+
+for i in range(1, n+1):         # 경유지
+    for j in range(1, n+1):     # 출발지
+        for k in range(1, n+1): # 도착지
+            # 출발-도착 > 출발-경유 + 경유-도착 -> 업데이트
+            if grp[j][k] > grp[j][i] + grp[i][k]:
+                grp[j][k] = grp[j][i] + grp[i][k]
+
+result = 0
+
+for i in range(1, n+1):
+    cnt=0
+    for j in range(1, n+1):
+        if grp[i][j] != float('inf') or grp[j][i] != float('inf'):
+            cnt+=1
+
+    if cnt == n-1:
+        result+=1
+
+print(result)
