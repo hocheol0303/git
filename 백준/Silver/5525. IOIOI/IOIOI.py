8a161bf7c25f8fca63ba8b1db5f0b90490372332
hocheol0303 <136863755+hocheol0303@users.noreply.github.com>
2024-09-11 08:52:34 +0900

    [Silver I] Title: IOIOI, Time: 348 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -5,36 +5,29 @@ s: 문자열
 '''
 
 import sys
+
 n=int(sys.stdin.readline())
 m=int(sys.stdin.readline())
 s=sys.stdin.readline().rstrip()
 
-p=list('IO'*n)
-p.append('I')
-p=''.join(p)
-
-tmp=0
-result=0
+# 컨닝
+p='IOI'
 index=0
-while True:
-    if index != 0 and index == tmp:
-        break
-    else:
-        tmp=index
-    if p in s[index:]:
-        index+=s[index:].index(p)+2
-        result+=1
-    else:
-        break
-print(result)
+iters=0
+result=0
 
-# result=0
-# index=0
-# while True:
-#     if p in s:
-#         index=s.index(p)+2
-#         result+=1
-#         s=s[index:]
-#     else:
-#         break
-# print(result)
+# 뭔지 알겠는데 50점 짜리가 왜 50점짜린지 모르겠음 이게 더 빨라?
+while index<(m-1):
+    # IOI의 개수를 세
+    # IOIOI로 쭊쭊쭈꾸ㅉ꾸쭊 이어져 있으면 맨 앞의 IO 제외해가면서 얼마나 연결되어 있는지 아세요
+    # 그 개수가 n과 같으면 답 += 1
+    if s[index:index+3] == p:
+        index+=2
+        iters+=1
+        if iters==n:
+            result+=1
+            iters-=1
+    else:
+        index+=1
+        iters=0
+print(result)
