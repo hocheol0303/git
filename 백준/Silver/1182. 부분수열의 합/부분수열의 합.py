cd3f0491aab77561bd813579d5c854990a916a22
hocheol0303 <136863755+hocheol0303@users.noreply.github.com>
2024-10-26 11:33:41 +0900

    [Silver II] Title: 부분수열의 합, Time: 552 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,37 @@
+'''
+n개의 정수로 이루어진 수열에서
+원소의 합이 s인 부분수열의 개수
+
+부분수열 중복 안받아?
+5 -2
+-1 -1 -1 -1 -1
+에 대해서 1이 정답임? 함 해보자 아니네
+
+5 0
+0 0 0 0 0
+정답: 31
+이거 어케하냐
+'''
+
+def dfs(start):
+    global n, s, hi, ans, check
+    if s == sum(hi) and len(hi) != 0 :
+       ans += 1 
+    #    check.add(tuple(hi))
+    #    print(*hi)
+    # else: # else가 아니래!!~!@!!~$#ㄸ@!~#@~@!~~@#!~#@ 신세계
+    for i in range(start, n):
+        hi.append(lst[i])
+        dfs(i+1)
+        hi.pop()
+
+import sys
+
+n, s = map(int, sys.stdin.readline().split())
+lst = list(map(int, sys.stdin.readline().split()))
+hi = []
+ans = 0
+# check = set()
+
+dfs(0)
+print(ans)
